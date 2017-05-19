c07f041a04fc682496a7decaadceac89871a6407
Maxim Smirnov <maxim.smirnov@mercurydevelopment.com>
2017-05-19 16:24:09 +0400

    Remove headerDecor as dependency (should be separate)

--- app/src/main/java/com/cuttingedge/PokeApp/Backpack/BackpackActivity.java
+++ app/src/main/java/com/cuttingedge/PokeApp/Backpack/BackpackActivity.java
@@ -1,10 +1,13 @@
 package com.cuttingedge.PokeApp.Backpack;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -14,9 +17,9 @@ import com.cuttingedge.PokeApp.BaseActivity;
 import com.cuttingedge.PokeApp.Pokedex;
 import com.cuttingedge.PokeApp.Pokemon;
 import com.cuttingedge.PokeApp.R;
+import com.cuttingedge.adapter2recycler.Adapter.ModularAdapter;
+import com.cuttingedge.adapter2recycler.Adapter.ModularAdapterBuilder;
 import com.cuttingedge.adapter2recycler.ModularItem;
-
-import ca.barrenechea.widget.recyclerview.decoration.StickyHeaderDecoration;
 
 /**
  * Created by Robbe Sneyders
@@ -25,7 +28,7 @@ import ca.barrenechea.widget.recyclerview.decoration.StickyHeaderDecoration;
  */
 public class BackpackActivity extends BaseActivity {
 
-    private PokemonInlineStickerAdapter adapter;
+    private ModularAdapter<ViewHolder, ModularItem> adapter;
     private boolean isAlphabetic;
 
     FloatingActionButton fab;
@@ -55,16 +58,17 @@ public class BackpackActivity extends BaseActivity {
 
         List<Pokemon> pokemonList = Pokedex.getAllPokemonAlphabetic();
         isAlphabetic = true;
-        List<ModularItem> list = formDataWithHeaders(pokemonList);
+        List<ModularItem> list = addHeaders(pokemonList);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        adapter = new PokemonInlineStickerAdapter.Builder<>(recyclerView, list).build();
+        adapter = new ModularAdapterBuilder<>(recyclerView, list)
+                .setSwipeLeft(Color.RED, ResourcesCompat.getDrawable(getResources(), R.drawable.ic_delete_white_24dp, null))
+                .setSwipeRight(Color.GREEN, ResourcesCompat.getDrawable(getResources(), R.drawable.ic_cloud_upload_white_24dp, null))
+                .build();
 
         new PokemonBackPackModule(this).bindToAdapter(adapter);
-
-        StickyHeaderDecoration decor = new StickyHeaderDecoration(adapter, true);
-        recyclerView.addItemDecoration(decor);
+        new HeaderModule().bindToAdapter(adapter);
     }
 
 
@@ -83,7 +87,7 @@ public class BackpackActivity extends BaseActivity {
             isAlphabetic = true;
         }
 
-        List<ModularItem> newList = formDataWithHeaders(pokemonList);
+        List<ModularItem> newList = addHeaders(pokemonList);
         List<ModularItem> oldList = adapter.getList();
         DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffCallback(oldList, newList));
         adapter.swap(newList, false);
@@ -91,46 +95,38 @@ public class BackpackActivity extends BaseActivity {
     }
 
 
-    public List<ModularItem> formDataWithHeaders(List<Pokemon> list) {
+    /**
+     * Adds alphabetic or type headers to a list of pokemon.
+     *
+     * @param list List of pokemon to which headers should be added.
+     * @return List with headers inserted.
+     */
+    public List<ModularItem> addHeaders(List<Pokemon> list) {
         List<ModularItem> newList = new ArrayList<>();
         if (list.size() == 0)
             return newList;
 
-        newList.add(list.get(1));
-        newList.add(list.get(2));
-        newList.add(list.get(3));
-        newList.add(list.get(4));
-        newList.add(list.get(5));
-        newList.add(list.get(6));
-        newList.add(list.get(7));
-        newList.add(list.get(8));
-        newList.add(list.get(9));
-        newList.add(list.get(10));
-        newList.add(list.get(11));
-        newList.add(list.get(12));
-        newList.add(list.get(13));
-        newList.add(list.get(14));
-        newList.add(list.get(15));
-//        if (isAlphabetic) {
-//            for (Pokemon pokemon: list) {
-//                if (!pokemon.name.substring(0, 1).toUpperCase().equals(header)) {
-//                    header = pokemon.name.substring(0, 1).toUpperCase();
-//                    newList.add(new Header(header));
-//                    newList.add(pokemon);
-//                } else
-//                    newList.add(pokemon);
-//            }
-//        }
-//        else {
-//            for (Pokemon pokemon: list) {
-//                if (!pokemon.type.toUpperCase().equals(header)) {
-//                    header = pokemon.type.toUpperCase();
-//                    newList.add(new Header(header));
-//                    newList.add(pokemon);
-//                } else
-//                    newList.add(pokemon);
-//            }
-//        }
+        String header = "";
+        if (isAlphabetic) {
+            for (Pokemon pokemon: list) {
+                if (!pokemon.name.substring(0, 1).toUpperCase().equals(header)) {
+                    header = pokemon.name.substring(0, 1).toUpperCase();
+                    newList.add(new Header(header));
+                    newList.add(pokemon);
+                } else
+                    newList.add(pokemon);
+            }
+        }
+        else {
+            for (Pokemon pokemon: list) {
+                if (!pokemon.type.toUpperCase().equals(header)) {
+                    header = pokemon.type.toUpperCase();
+                    newList.add(new Header(header));
+                    newList.add(pokemon);
+                } else
+                    newList.add(pokemon);
+            }
+        }
         return newList;
     }
 
